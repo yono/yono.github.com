006032572868ef81baababab58ce71980420ee4a
yono <yono.05@gmail.com>
2010-05-18 13:10:07 +0900

    python/ 以下を修正

--- python/python_basics/scripts/create-elements.js
+++
@@ -1,7 +0,0 @@
-document.createElement('header');
-document.createElement('aside');
-document.createElement('article');
-document.createElement('footer');
-document.createElement('section');
-document.createElement('figure');
-document.createElement('nav');
